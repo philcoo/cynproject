358d0df3894084a2ff7592b9e50b3cd95d2433b6
Philco Michel <phil9231@gmail.com>
2016-08-23 18:21:33 -0400

    home page and about page is updated

---
+++ db/migrate/20160816204605_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[5.0]
+  def change
+    create_table :orders do |t|
+      t.string :name
+      t.string :email
+      t.integer :phone
+
+      t.timestamps
+    end
+  end
+end
